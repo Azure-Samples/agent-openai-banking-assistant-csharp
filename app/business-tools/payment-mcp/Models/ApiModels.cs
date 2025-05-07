b13a8896cb4018bff1a7a0f1f92e19a37dff431e
Johnson Tseng <vtjc2002@gmail.com>
2025-05-07 12:28:05 -0400

    refactor for DI and use MCP

---
+++ app/business-tools/payment-mcp/Models/ApiModels.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+
+public record Payment
+{
+    public required string AccountId { get; init; }
+    public required string Description { get; init; }
+    public string? PaymentType { get; init; }
+    public string? PaymentMethodId { get; init; }
+    public required string RecipientName { get; init; }
+    public required string RecipientBankCode { get; init; }
+    public required string Amount { get; init; }
+    public required string Timestamp { get; init; }
+}
+
+public record Transaction
+{
+    public string Id { get; init; }
+    public string Description { get; init; }
+    public string Type { get; init; }
+    public string RecipientName { get; init; }
+    public string RecipientBankCode { get; init; }
+    public string AccountId { get; init; }
+    public string PaymentType { get; init; }
+    public string? Amount { get; init; }
+    public string? Timestamp { get; init; }
+}
